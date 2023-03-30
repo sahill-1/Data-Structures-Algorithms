6492df5f330ee5512a9c5acf64223fe6609b1924
sahill-1 <sv260397@gmail.com>
2023-03-30 12:05:28 +0530

    reversal algo array rotation

--- ReversalAlgoArrayRotation.js
+++ ReversalAlgoArrayRotation.js
@@ -1,30 +1,31 @@
-// Given an array arr[] of size N, the task is to rotate the array by d position to the left.
+// Reversal algorithm for Array rotation
+// Given an array arr[] of size N, the task is to rotate the array by d position to the left
 
 function reverseArray(arr, start, end){
     while(start<end){
         let temp = arr[start];
         arr[start] = arr[end];
         arr[end] = temp;
+
         start++;
         end--;
     }
 }
-function leftRotate(arr, d, n){
-    if(d===0) return;
-    d = d % n;
 
+function reversalAlgo(arr, n, d){
+    if (d===0) return;
+
+    d = d % n;  //incase rotating factor is greater than array length
+    
     reverseArray(arr, 0, d-1);
     reverseArray(arr, d, n-1);
-    reverseArray(arr, 0, n-1)
+    reverseArray(arr, 0, n-1);
+
+    console.log(arr);
 }
-function printarray(arr,size){
-    for (let i=0; i<size; i++){
-        console.log(arr[i])
-    }
-}
-let arr = [1,2,3,4,5,6];
+
+let arr = [1,2,3,4,5,6,7];
 let n = arr.length;
 let d = 2;
 
-leftRotate(arr,d,n)
-printarray(arr,n)
+reversalAlgo(arr, n ,d)
--- reverse_array.js
+++ reverse_array.js
@@ -10,3 +10,7 @@ function reverseArray(arr){
 
 let arr = [1,2,3,4,5,6,7,8,9,10];
 reverseArray(arr)
+
+
+
+
