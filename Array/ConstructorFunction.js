cee506254258c4d69aa310af3f8ae8a96ff8a912
sahill-1 <sv260397@gmail.com>
2023-04-12 14:38:04 +0530

    Constructor Function

---
+++ Array/ConstructorFunction.js
@@ -0,0 +1,25 @@
+// Constructor Function
+// How to use them
+
+// let student1 = {
+//     firstName : "Sahil",
+//     lastName : "Vishwakarma",
+//     age : 25,
+//     job : "Frontend Developer"
+// }
+
+// Object Constructor
+
+function Student(first, last, age, job){
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.job = job;
+}
+
+let student1 = new Student("Sahil", "Vishwakarma", 25, "Frontend Developer");
+student1.nationality = "Indian"
+student1.name = function (){
+    return this.firstName + " " +this.lastName;
+}
+console.log(student1.name());
